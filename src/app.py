63dd45a9f5b35a08938f001e2a4344da3faf0869
jchuahtacc <jchuah@tacc.utexas.edu>
2021-04-08 11:46:31 -0500

    Fix missing import

--- src/app.py
+++ src/app.py
@@ -24,6 +24,7 @@ import dash_html_components as html
 import dash_bootstrap_components as dbc
 import dash_table as dt
 import dash_daq as daq
+from flask import request
 from dash.exceptions import PreventUpdate
 from dash.dependencies import Input, Output, State, ALL, MATCH
 
